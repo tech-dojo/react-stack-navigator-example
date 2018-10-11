ee48797c6f88133b8164387cb7851174cd5e9832
Your Name <you@example.com>
2018-10-11 13:49:32 +0600

    Working..

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import { AppRegistry } from 'react-native';
 
-//import MainDrawer from './smart/Drawer' ;
 import  StackNav from './StackNavigator/StackNav' ;
 
-/// Smart Grocery Native Drawer
-//   AppRegistry.registerComponent('smartGroceryNative', () => MainDrawer);
-
 /// Stack Navigator
-  AppRegistry.registerComponent('smartGroceryNative', () => StackNav);
+AppRegistry.registerComponent('smartGroceryNative', () => StackNav);
