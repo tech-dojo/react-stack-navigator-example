e0c17fbfdd2f9d15234dfa9b2803c21f5a325dc4
Your Name <you@example.com>
2018-10-12 22:25:25 +0600

    Done

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-import { AppRegistry ,YellowBox} from 'react-native';
-
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
-
+import { AppRegistry } from 'react-native';
 import  StackNav from './StackNavigator/StackNav' ;
 
 /// Stack Navigator
